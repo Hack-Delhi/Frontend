6e191e1ffde238eb4c64543aad31cb8d0c801efa
championshuttler <shivams2799@gmail.com>
2019-01-30 18:52:29 +0530

    update

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -100,7 +100,7 @@ __webpack_require__.r(__webpack_exports__);
 
 // set typed
 var typed = new Typed('#typed', {
-  strings: ['// code for social good'],
+  strings: ['Powered by UltraHack'],
   typeSpeed: 30,
   backSpeed: 8,
   backDelay: 3500,
